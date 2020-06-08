a90896a74609dcba21f546a000b1930e9bc4fbe9
shivraj kumar <shivrajkumarcu@gmail.com>
2020-06-08 19:12:16 +0530

    handle 0 1 and 2 number sum

---
+++ StringCalculator/src/stringCalculator/TestCalculator.java
@@ -0,0 +1,26 @@
+package stringCalculator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestCalculator {
+	
+	@Test
+	public void emptyStringReturn0()
+	{
+		assertEquals(0,Calculator.add(""));
+	}
+	
+	@Test
+	public void numberOfLengthOne()
+	{
+		assertEquals(1, Calculator.add("1"));
+	}
+	
+	@Test
+	public void sumOfNumberSeperatedByComma()
+	{
+		assertEquals(5, Calculator.add("2,3"));
+	}
+
+}
